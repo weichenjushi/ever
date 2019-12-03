e302b350d8caa67d3c85522d358d269ba5c18047
wangluhui <wangluhui@yidian-inc.com>
2019-12-03 14:52:17 +0800

    update conf.py

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,6 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'chinese_search'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
